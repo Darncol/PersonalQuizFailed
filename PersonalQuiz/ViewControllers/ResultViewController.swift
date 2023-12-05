796aa6a0bd2474da7f3b371a5e9608535d334ae1
Darncol <60666804+Darncol@users.noreply.github.com>
2023-12-05 19:49:33 +0200

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 29.11.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var describeLabel: UILabel!
+    
+    var answers: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        
+        updateUI()
+    }
+    
+    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    private func mostFrequentAnimal() -> Animal? {
+        answers.reduce(into: [Animal: Int]()) { $0[$1.animal, default: 0] += 1 }
+            .max(by: { $0.value < $1.value })?
+            .key
+    }
+    
+    private func updateUI() {
+        guard let animal = mostFrequentAnimal() else { return }
+        
+        emojiLabel.text = "Вы \(animal.rawValue.description)"
+        describeLabel.text = animal.definition
+    }
+}
